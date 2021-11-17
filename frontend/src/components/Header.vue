e0173e1a29070cffdf300b226b2be9c537fb0661
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2021-11-17 21:18:02 +0100

    add header and navigation

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,79 @@
+<template>
+<ion-app>
+  <ion-menu side="end" content-id="main-content">
+    <ion-header>
+      <ion-toolbar translucent>
+        <ion-title>Menu</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-list>
+        <ion-item>
+          <ion-icon :icon="personCircle" slot="start"></ion-icon>
+          <ion-label>Profil</ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-icon :icon="bagCheck" slot="start"></ion-icon>
+          <ion-label>Bestellen</ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-icon :icon="cart" slot="start"></ion-icon>
+          <ion-label>Einkaufen &#38; Liefern</ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-icon :icon="settings" slot="start"></ion-icon>
+          <ion-label>Einstellungen</ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-icon :icon="logOut" slot="start"></ion-icon>
+          <ion-label>Abmelden</ion-label>
+        </ion-item>
+      </ion-list>
+    </ion-content>
+  </ion-menu>
+
+  <div id="main-content">
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="end">
+          <ion-menu-button></ion-menu-button>
+        </ion-buttons>
+        <ion-title>{{ title }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+  </div>
+</ion-app>
+</template>
+
+<script>
+import {
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonList,
+  IonMenu,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
+import { bagCheck, cart, logOut, personCircle, settings } from "ionicons/icons";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {
+    IonContent,
+    IonHeader,
+    IonItem,
+    IonList,
+    IonMenu,
+    IonTitle,
+    IonToolbar,
+    IonIcon,
+  },
+  setup() {
+    return { personCircle, cart, bagCheck, settings, logOut };
+  },
+
+  props: ["title"],
+});
+</script>
