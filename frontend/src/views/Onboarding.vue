b356d50b669ab5ca4d537775cdf9babc48169cae
Wecks Salome Amadea <sw193@hdm-stuttgart.de>
2021-11-19 10:03:34 +0100

    onboarding try out

--- frontend/src/views/Onboarding.vue
+++ frontend/src/views/Onboarding.vue
@@ -1,12 +1,45 @@
 <template>
   <ion-page>
+    <ion-header translucent>
+      <ion-toolbar>
+        <ion-title>So funktioniert Goodies</ion-title>
+      </ion-toolbar>
+    </ion-header>
     <ion-content>
       <swiper :pagination="{ clickable: true }">
-        <swiper-slide>Slide 1</swiper-slide>
-        <swiper-slide>Slide 2</swiper-slide>
-        <swiper-slide>Slide 3</swiper-slide>
-        <swiper-slide>Slide 4</swiper-slide>
-        <swiper-slide>Slide 5</swiper-slide>
+        <swiper-slide>
+          <ion-card>
+            <img src="../assets/orderPic.png" />
+            <ion-card-title>Kreiere deine Einkaufsliste</ion-card-title>
+            <ion-card-content>
+              Mit Goodies kannst du dir Lebensmittel von zuhause aus bestellen und liefern lassen. 
+              Über den Bestellen Button gibst du hierfür deine Einkaufsliste an und beschreibst deine Artikel möglichst genau.  
+            </ion-card-content>
+          </ion-card>
+        </swiper-slide>
+        <swiper-slide>
+          <ion-card>
+            <img src="../assets/deliveryPic.png" />
+            <ion-card-title>Tu etwas Gutes</ion-card-title>
+            <ion-card-content>
+              Als Lieferant suchst du dir eine passende Bestellung in deiner Nähe, stellst sie zusammen 
+              und lieferst sie gegen eine Belohnung aus. Denke daran den Kassenzettel mitzunehmen.  
+            </ion-card-content>
+          </ion-card>
+        </swiper-slide>
+        <swiper-slide>
+          <ion-card>
+            <ion-card-title>Tu etwas Gutes</ion-card-title>
+            <ion-card-content>
+              Auf der Karte siehst du als Besteller immer wo sich deine Lieferung befindet. 
+            </ion-card-content>
+            <ion-card-content>
+            Ist deine Bestellung da, überprüfe den Kassenzettel und bezahle deinen Lieferanten - fertig!
+            </ion-card-content>
+            <ion-button @click="() => router.push('/home')"> Return
+            </ion-button>
+          </ion-card>
+        </swiper-slide>
       </swiper>
     </ion-content>
   </ion-page>
@@ -17,6 +50,7 @@ import { IonContent, IonPage, IonicSwiper } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import SwiperCore, { Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/vue';
+import { useRouter } from 'vue-router';
 
 import 'swiper/swiper-bundle.min.css';
 import '@ionic/vue/css/ionic-swiper.css';
@@ -31,11 +65,26 @@ export default defineComponent({
     Swiper,
     SwiperSlide,
   },
+  setup() {
+    return {
+      router: useRouter(),
+    };
+  },
 });
 </script>
 
 <style scoped>
-.swiper-container {
-  height: 100%;
+ion-card{
+  display: flex;
+  flex-direction: column;
+  width: 60% !important;
+  width: 60% !important;
+  margin: 200 !important;
+}
+
+img {
+  margin-left: auto;
+  margin-right: auto;
+  width: 80%;
 }
 </style>
