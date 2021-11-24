f3c80cc1e4bb1ba3cc1674e5443858f2a12dd569
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2021-11-24 15:01:38 +0100

    update linter config

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
     'vue/no-deprecated-slot-attribute': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     'vue/no-multiple-template-root': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
   },
   overrides: [
     {
