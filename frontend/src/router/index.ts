2d1b11069fcc7a70b82b9d56d695f9cf776ed381
mb309 <mb309@hdm-stuttgart.de>
2021-11-24 12:01:07 +0100

    git commit

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue';
 import Onboarding from '../views/Onboarding.vue';
+import LiefernHome from '../views/LiefernHome.vue';
+import BestellenHome from '../views/BestellenHome.vue';
 import Example from '@/views/Example.vue';
 
 const routes: Array<RouteRecordRaw> = [
@@ -18,6 +20,16 @@ const routes: Array<RouteRecordRaw> = [
     path: '/onboarding',
     name: 'Onboarding',
     component: Onboarding,
+  },
+  {
+    path: '/bestellen',
+    name: 'Bestellen Übersicht',
+    component: BestellenHome,
+  },
+  {
+    path: '/liefern',
+    name: 'Liefern Übersicht',
+    component: LiefernHome,
   },
   {
     path: '/example',
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,26 @@
 <template>
   <ion-page>
-    <Header title="Home" />
+    <Header></Header>
+
+    <ion-content>
+      <div id="ModusselectionBox">
+        <br>
+          <h4>Herzlich Willkommen bei goodies!</h4><!--set to H1 and change size of h1-->
+          <p>Hier helfen Nachbarn Nachbarn.</p>
+          
+          <p><strong>Triff deine Auswahl:</strong></p>
+          <p>Möchtst du<br><strong>Deinen Nachbarn unterstützen?</strong></p>
+          <router-link to="/liefern">
+            <img class="selectionbuttons" src="../assets/home/liefern.png">
+          </router-link>
+        <br>
+
+          <p>Oder möchtst du<br><strong>Du dir etwas liefern lassen?</strong></p>
+          <router-link to="/liefern">
+            <img class="selectionbuttons" src="../assets/home/bestellen.png">
+          </router-link>
+      </div>>
+    </ion-content>
   </ion-page>
 </template>
 
@@ -54,4 +74,19 @@ export default defineComponent({
 #container a {
   text-decoration: none;
 }
+
+#ModusselectionBox{
+  background-color: white;
+  text-align: center;
+  color: rgb(44, 44, 44);
+}
+
+.selectionbuttons{
+  position: relative;
+  text-align: center;
+  width: 70%;
+  max-width: 200px;
+}
+
+
 </style>
