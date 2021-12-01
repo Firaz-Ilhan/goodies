162e669aadcf7f2b0f3f7546691157318845a278
Chris <ch148@hdm-stuttgart.de>
2021-12-01 12:46:39 +0100

    add height and width to map div and merge main

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -1,44 +1,40 @@
 <template>
-  <div ref="mapDiv">
-  </div>
+  <div ref="mapDiv" style="width: 100%; height: 80vh"></div>
 </template>
 
 <script lang="ts">
-import {
-} from '@ionic/vue';
+import {} from '@ionic/vue';
 
 import { defineComponent, onMounted, ref } from 'vue';
-import { Loader } from '@googlemaps/js-api-loader'
+import { Loader } from '@googlemaps/js-api-loader';
 
 /* ToDo: 
 - Längen und Breitengrad: Firebase Datenbank-Abfrage
 - Dummie-Set erstellen? (öffentliches Fahrzeug?)
 */
 
-const API_KEY = "AIzaSyAeTmR_WD4BemHmW2vKV-V6Kc1_TbGs6QA"
+const API_KEY = 'AIzaSyAeTmR_WD4BemHmW2vKV-V6Kc1_TbGs6QA';
 
 export default defineComponent({
-name: "Map",
+  name: 'Map',
 
-setup() {
-  // const {coords} = useGeolocation()
-  const fixedPosition = { lat: 48.742857061431366, lng: 9.101005423756947 }
-  const loader = new Loader({apiKey: API_KEY})
-  const mapDiv = ref(null)
+  setup() {
+    // const {coords} = useGeolocation()
+    const fixedPosition = { lat: 48.742857061431366, lng: 9.101005423756947 };
+    const loader = new Loader({ apiKey: API_KEY });
+    const mapDiv = ref(null);
 
-
-  onMounted(async() => {
-    await loader.load().then((google) => {
-      new google.maps.Map(mapDiv.value, {
+    onMounted(async () => {
+      await loader.load().then((google) => {
+        new google.maps.Map(mapDiv.value, {
           center: fixedPosition,
-          zoom: 7
-        })
-    })
-  })
+          zoom: 7,
+        });
+      });
+    });
 
-  return{ mapDiv }
-}
-
+    return { mapDiv };
+  },
 });
 </script>
 
