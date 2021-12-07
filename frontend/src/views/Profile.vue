c1cb8d4a5de99d8273b03453ab40db1b4d386a81
Wecks Salome Amadea <sw193@hdm-stuttgart.de>
2021-12-07 15:49:16 +0100

    profile is completed

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -9,27 +9,27 @@
                             <div>PERSÖNLICHE ANGABEN</div>
                             <ion-item>
                                 <ion-label position="floating">Vorname</ion-label>
-                                <ion-input type="text" v-model="profile.firstname" required></ion-input>
+                                <ion-input type="text" v-model="profile.firstname" autocomplete="given-name" required></ion-input>
                             </ion-item>
                             <ion-item>
                                 <ion-label position="floating">Nachname</ion-label>
-                                <ion-input type="text" v-model="profile.lastname" required></ion-input>
+                                <ion-input type="text" v-model="profile.lastname" toaucomplete="family-name" required></ion-input>
                             </ion-item>
                             <ion-item>
                                 <ion-label position="floating">Straße</ion-label>
-                                <ion-input type="text" v-model="profile.street" required></ion-input>
+                                <ion-input type="text" v-model="profile.street" autocomplete="street-address" required></ion-input>
                             </ion-item>
                             <ion-item>
                                 <ion-label position="floating">Ort</ion-label>
-                                <ion-input type="text" v-model="profile.location" required></ion-input>
+                                <ion-input type="text" v-model="profile.location" autocomplete="street-address" required></ion-input>
                             </ion-item>
                             <ion-item>
                                 <ion-label position="floating">Postleitzahl</ion-label>
-                                <ion-input type="text" v-model="profile.postalcode" required></ion-input>
+                                <ion-input type="text" v-model="profile.postalcode" autocomplete="postal-code" required></ion-input>
                             </ion-item>
                             <ion-item>
                                 <ion-label position="floating">Telefon</ion-label>
-                                <ion-input type="text" v-model="profile.telephone" required></ion-input>
+                                <ion-input type="text" v-model="profile.telephone" autocomplete="tel" required></ion-input>
                             </ion-item>
                         </ion-card-content>
                     </ion-card>
@@ -37,14 +37,14 @@
                         <ion-card-content>
                         <div>ZAHLUNGSART</div>
                             <ion-list>
-                                <ion-radio-group>
+                                <ion-radio-group name="payment" v-model="profile.payment">
                                     <ion-item>
                                         <ion-label>Barzahlung</ion-label>
-                                        <ion-radio v-model="profile.cash" slot="start" value="barzahlung"></ion-radio>
+                                        <ion-radio  slot="start" value="barzahlung"></ion-radio>
                                     </ion-item>
                                     <ion-item>
                                         <ion-label>Paypal</ion-label>
-                                        <ion-radio v-model="profile.paypal" slot="start" value="paypal"></ion-radio>
+                                        <ion-radio  slot="start" value="paypal"></ion-radio>
                                     </ion-item>
                                 </ion-radio-group>
                             </ion-list>
@@ -71,8 +71,9 @@ import {
     IonButton,
 } from '@ionic/vue';
 import { db } from '../main';
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, toRefs } from 'vue';
 import firebase from 'firebase';
+import {IProfile} from '../interfaces/IProfile';
 import Header from '../components/Header.vue';
 
 export default defineComponent({
@@ -91,26 +92,49 @@ export default defineComponent({
     IonButton,
   },
   
-  data() {
+  setup() {
+
+      const state = reactive({ 
+          profile: {
+            firstname: '',
+            lastname: '',
+            street: '',
+            location: '',
+            postalcode: '',
+            telephone: '', 
+            payment: 'barzahlung',
+          }
+        });
+
+      const saveProfile = () => {
+        const currentUser =  firebase.auth().currentUser!;
+        console.log("Profile",state.profile)
+        db.collection('profiles').doc(currentUser.uid).set({...state.profile});
+      };
+
+     const getProfileData = () => {
+        const currentUser =  firebase.auth().currentUser!;
+        console.log("Profile", state.profile)
+        db.collection('profiles').doc(currentUser.uid).get().then((doc) => {
+            if (doc.exists) {
+                console.log("Document data:", doc.data());
+                state.profile = doc.data() as IProfile
+            } else {
+                console.log("No such document!");
+            }
+        }).catch((error) => {
+            console.log("Error getting document:", error);
+        });
+      };
+
+      getProfileData()
+
     return {
-      profile: {
-        firstname: '',
-        lastname: '',
-        street: '',
-        location: '',
-        postalcode: '',
-        telephone: '',
-        cash: false,
-        paypal: true,
-      },
+      ...toRefs(state),
+      saveProfile,
     };
-  },
-  methods: {
-    saveProfile() {
-        console.log(`saved, ${this.profile.firstname}`)
-        
-  }
-  }
+    
+},
 });
 </script>
 
