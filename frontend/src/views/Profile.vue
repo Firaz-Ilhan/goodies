249d5c8450146027d52e0e56b8d36fc92303b983
Wecks Salome Amadea <sw193@hdm-stuttgart.de>
2021-12-07 13:40:41 +0100

    v-model changes

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -2,34 +2,34 @@
     <ion-page>
         <Header title="Profil" :hasBackButton="true" ></Header>
         <ion-content>
-            <form>
+            <form @submit.prevent="saveProfile">
                 <div class="wrapper">
                     <ion-card>
                         <ion-card-content>
                             <div>PERSÖNLICHE ANGABEN</div>
                             <ion-item>
                                 <ion-label position="floating">Vorname</ion-label>
-                                <ion-input required></ion-input>
+                                <ion-input type="text" v-model="profile.firstname" required></ion-input>
                             </ion-item>
                             <ion-item>
                                 <ion-label position="floating">Nachname</ion-label>
-                                <ion-input required></ion-input>
+                                <ion-input type="text" v-model="profile.lastname" required></ion-input>
                             </ion-item>
                             <ion-item>
                                 <ion-label position="floating">Straße</ion-label>
-                                <ion-input required></ion-input>
+                                <ion-input type="text" v-model="profile.street" required></ion-input>
                             </ion-item>
                             <ion-item>
                                 <ion-label position="floating">Ort</ion-label>
-                                <ion-input required></ion-input>
+                                <ion-input type="text" v-model="profile.location" required></ion-input>
                             </ion-item>
                             <ion-item>
                                 <ion-label position="floating">Postleitzahl</ion-label>
-                                <ion-input required></ion-input>
+                                <ion-input type="text" v-model="profile.postalcode" required></ion-input>
                             </ion-item>
                             <ion-item>
                                 <ion-label position="floating">Telefon</ion-label>
-                                <ion-input required></ion-input>
+                                <ion-input type="text" v-model="profile.telephone" required></ion-input>
                             </ion-item>
                         </ion-card-content>
                     </ion-card>
@@ -40,11 +40,11 @@
                                 <ion-radio-group>
                                     <ion-item>
                                         <ion-label>Barzahlung</ion-label>
-                                        <ion-radio slot="start" value="barzahlung"></ion-radio>
+                                        <ion-radio v-model="profile.cash" slot="start" value="barzahlung"></ion-radio>
                                     </ion-item>
                                     <ion-item>
                                         <ion-label>Paypal</ion-label>
-                                        <ion-radio slot="start" value="paypal"></ion-radio>
+                                        <ion-radio v-model="profile.paypal" slot="start" value="paypal"></ion-radio>
                                     </ion-item>
                                 </ion-radio-group>
                             </ion-list>
@@ -59,34 +59,57 @@
 
 <script lang="ts">
 import {
-  IonContent,
-  IonCard,
-  IonLabel,
-  IonInput,
-  IonItem,
-  IonCardContent,
-  IonRadioGroup,
-  IonRadio,
-  IonList,
-  IonButton,
+    IonContent,
+    IonCard,
+    IonLabel,
+    IonInput,
+    IonItem,
+    IonCardContent,
+    IonRadioGroup,
+    IonRadio,
+    IonList,
+    IonButton,
 } from '@ionic/vue';
+import { db } from '../main';
 import { defineComponent } from 'vue';
+import firebase from 'firebase';
 import Header from '../components/Header.vue';
 
 export default defineComponent({
   name: 'Profile',
   components: {
-      Header,
-        IonContent,
-        IonLabel,
-        IonInput,
-        IonItem,
-        IonCard,
-        IonCardContent,
-        IonRadioGroup,
-        IonRadio,
-        IonList,
-        IonButton,
+    Header,
+    IonContent,
+    IonLabel,
+    IonInput,
+    IonItem,
+    IonCard,
+    IonCardContent,
+    IonRadioGroup,
+    IonRadio,
+    IonList,
+    IonButton,
+  },
+  
+  data() {
+    return {
+      profile: {
+        firstname: '',
+        lastname: '',
+        street: '',
+        location: '',
+        postalcode: '',
+        telephone: '',
+        cash: false,
+        paypal: true,
+      },
+    };
+  },
+  methods: {
+    saveProfile() {
+        console.log(`saved, ${this.profile.firstname}`)
+        
+  }
   }
 });
 </script>
