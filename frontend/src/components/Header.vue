665d1e50de33d31afda1257d0ec81dd4d2902a5b
mb309 <mb309@hdm-stuttgart.de>
2021-12-11 11:40:58 +0100

    DOMException: Failed to execute 'setAttribute' on 'Element': '0' is not a valid attribute name.

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,5 +1,6 @@
 <template>
   <ion-header class="ion-margin-bottom">
+    <img v-bind;´:src="menueBgImg()" class="menue-bg-img">
     <ion-toolbar>
       <ion-buttons slot="start" v-if="hasBackButton">
         <ion-back-button default-href="/home"></ion-back-button>
@@ -22,6 +23,7 @@ import {
   IonBackButton,
 } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import {useRoute} from 'vue-router';
 
 export default defineComponent({
   name: 'Header',
@@ -33,6 +35,21 @@ export default defineComponent({
     IonMenuButton,
     IonBackButton,
   },
+  setup() {
+    const route = useRoute()
+    function menueBgImg() { 
+      const currentRoute= route.path; 
+      console.log(currentRoute);
+
+      switch(currentRoute){ 
+        case "/home" || "/" || "/register" || "/login" || "/onboarding": return "require('@/assets/menueBgDelivery.svg')"; //  
+        case "/bestellen": return "require('@/assets/images/menueBgOrder.svg')"; 
+        case "/liefern": return "require('@/assets/images/menueBgDelivery.svg')"; 
+        default: return "require('@/assets/images/menue_bg_deliver.svg')"; 
+      } 
+    }
+     return { menueBgImg }
+  }, 
   props: { title: String, hasBackButton: Boolean },
 });
 </script>
