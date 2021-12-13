8a277a5d31e276cd99b083b1a4e69db0ec26119a
Chris <ch148@hdm-stuttgart.de>
2021-12-13 09:52:23 +0100

    refactor outsource addListItem

---
+++ frontend/src/components/AddListItem.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="container">
+    <h1 class="caption">Artikel hinzufügen</h1>
+    <ion-row class="ion-align-items-end">
+      <ion-col size="6">
+        <ion-item>
+          <ion-label position="floating">Artikel</ion-label>
+          <ion-input v-model="article" autofocus></ion-input>
+        </ion-item>
+      </ion-col>
+      <ion-col size="3">
+        <ion-item>
+          <ion-label position="floating">Anzahl</ion-label>
+          <ion-input v-model="amount" required></ion-input>
+        </ion-item>
+      </ion-col>
+      <ion-col size="1">
+        <ion-button
+          @click="addEntryToList"
+          fill="clear"
+          shape="round"
+          :disabled="article.length <= 0"
+        >
+          <ion-icon slot="icon-only" :icon="addCircleOutline"></ion-icon>
+        </ion-button>
+      </ion-col>
+    </ion-row>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  IonButton,
+  IonLabel,
+  IonInput,
+  IonItem,
+  IonCol,
+  IonRow,
+  IonIcon,
+} from '@ionic/vue';
+import { addCircleOutline } from 'ionicons/icons';
+import { defineComponent } from 'vue';
+import { IListEntry } from '../interfaces/IListEntry';
+
+export default defineComponent({
+  name: 'AddListItem',
+  props: ['setList'],
+  methods: {
+    addEntryToList() {
+      const listEntry: Omit<IListEntry, 'id'> = {
+        article: this.article,
+        amount: this.amount,
+        isChecked: false,
+      };
+
+      if (this.article && this.amount > 0) {
+        this.list.push(listEntry);
+        this.article = '';
+        this.amount = 1;
+        this.setList(this.list);
+      } else {
+        console.log('invalid input');
+      }
+    },
+  },
+  components: {
+    IonButton,
+    IonLabel,
+    IonInput,
+    IonItem,
+    IonCol,
+    IonRow,
+    IonIcon,
+  },
+  data() {
+    return {
+      name: '',
+      article: '',
+      amount: 1,
+      list: new Array<IListEntry>(),
+      addCircleOutline,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.caption {
+  margin-top: 30px;
+}
+</style>
--- frontend/src/components/CreateOrderModal.vue
+++ frontend/src/components/CreateOrderModal.vue
@@ -1,7 +1,7 @@
 <template>
   <ion-header>
     <ion-toolbar>
-      <ion-title>{{ title }}</ion-title>
+      <ion-title>Bestellung anlegen</ion-title>
       <ion-buttons slot="end">
         <ion-button @click="closeModal">Schließen</ion-button>
       </ion-buttons>
@@ -9,21 +9,22 @@
   </ion-header>
 
   <ion-content>
-    <form class="wrapper" @submit.prevent="createOrder">
-      <span>Wie möchtest du deine Bestellung nennen?</span>
+    <form @submit.prevent="createOrder" class="wrapper">
+      <h1 class="caption">Deine Bestellung</h1>
 
       <ion-item>
         <ion-label position="floating">Name</ion-label>
         <ion-input v-model="name" required />
       </ion-item>
 
-      <!-- TODO outsource -->
+      <AddListItem :setList="setList" />
+
       <ion-list v-if="list.length > 0">
+        <h1 class="caption">Einkaufsliste</h1>
         <ion-item class="list-header">
           <ion-col size="9">Artikel</ion-col>
-          <ion-col size="3">Menge</ion-col>
+          <ion-col size="3">Anzahl</ion-col>
         </ion-item>
-
         <ion-item-sliding v-for="(entry, index) in list" :key="index">
           <ion-item-options side="end">
             <ion-item-option @click="removeFromList(index)" color="danger">
@@ -37,35 +38,11 @@
         </ion-item-sliding>
       </ion-list>
 
-      <ion-list>
-        <span>Artikel hinzufügen</span>
-        <ion-row class="ion-align-items-end">
-          <ion-col size="6">
-            <ion-item>
-              <ion-label position="floating">Artikel</ion-label>
-              <ion-input v-model="article" autofocus></ion-input>
-            </ion-item>
-          </ion-col>
-          <ion-col size="3">
-            <ion-item>
-              <ion-label position="floating">Menge</ion-label>
-              <ion-input v-model="amount" required></ion-input>
-            </ion-item>
-          </ion-col>
-          <ion-col size="1">
-            <ion-button
-              @click="addEntryToList"
-              fill="clear"
-              shape="round"
-              :disabled="article.length <= 0"
-            >
-              <ion-icon slot="icon-only" :icon="addCircleOutline"></ion-icon>
-            </ion-button>
-          </ion-col>
-        </ion-row>
-      </ion-list>
-
-      <ion-button class="btn-center" type="submit" :disabled="list.length <= 0">
+      <ion-button
+        class="btn-center create-btn"
+        type="submit"
+        :disabled="list.length <= 0"
+      >
         Erstellen
       </ion-button>
     </form>
@@ -86,7 +63,6 @@ import {
   IonItem,
   IonList,
   IonCol,
-  IonRow,
   IonIcon,
   IonItemOptions,
   IonItemOption,
@@ -97,13 +73,11 @@ import { addCircleOutline, trashOutline } from 'ionicons/icons';
 import firebase from 'firebase';
 import { defineComponent } from 'vue';
 import { IListEntry } from '../interfaces/IListEntry';
-import { IOrder } from '@/interfaces/IOrder';
+import { IOrder } from '../interfaces/IOrder';
+import AddListItem from './AddListItem.vue';
 
 export default defineComponent({
   name: 'CreateOrderModal',
-  props: {
-    title: String,
-  },
   methods: {
     async closeModal() {
       await modalController.dismiss();
@@ -130,23 +104,6 @@ export default defineComponent({
           });
       }
     },
-
-    addEntryToList() {
-      const listEntry: Omit<IListEntry, 'id'> = {
-        article: this.article,
-        amount: this.amount,
-        isChecked: false,
-      };
-
-      if (this.article && this.amount > 0) {
-        this.list.push(listEntry);
-        this.article = '';
-        this.amount = 1;
-      } else {
-        console.log('invalid input');
-      }
-    },
-
     removeFromList(index: number) {
       this.list.splice(index, 1);
 
@@ -154,6 +111,9 @@ export default defineComponent({
       document.querySelectorAll('ion-item-sliding').forEach((el) => {
         el.close();
       });
+    },
+    setList(list: IListEntry[]) {
+      this.list = list;
     },
   },
   components: {
@@ -168,49 +128,42 @@ export default defineComponent({
     IonItem,
     IonList,
     IonCol,
-    IonRow,
     IonIcon,
     IonItemOptions,
     IonItemOption,
     IonItemSliding,
+    AddListItem,
   },
   data() {
     return {
       name: '',
-      article: '',
-      amount: 1,
       list: new Array<IListEntry>(),
       addCircleOutline,
       trashOutline,
+      AddListItem,
     };
   },
 });
 </script>
 
 <style scoped lang="scss">
-h1 {
-  margin-top: 30px;
-  margin-bottom: 0;
-  font-size: 24px;
-}
 .list-header {
-  font-family: Rubik;
-  font-size: 16px;
+  font-size: 14px;
+  // color: var(--ion-color-medium);
+  margin-top: 30px;
   ion-col {
     padding: 0;
   }
 }
 
 form {
+  margin: 20px 0;
+}
+
+.btn-center {
   margin-top: 30px;
 }
-
-ion-list {
-  margin: 40px 0;
-}
-
-span {
-  font-family: Rubik;
-  font-weight: 500;
+.caption {
+  margin-top: 30px;
 }
 </style>
