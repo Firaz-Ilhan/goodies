014da903e0376b427689aeb1982b01a45353e748
mb309 <mb309@hdm-stuttgart.de>
2022-02-17 23:16:57 +0100

    Limited the ripple effect to the button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,25 +13,24 @@
     
         <router-link to="/deliveries">
           <div class="ion-activatable ripple-parent">
-           <img src="../assets/images/liefern_orange.png"  alt="Einkaufen und Liefern">
-           <div class="text_bottom_centered"> Einkaufen & <br/> Liefern</div>
+          <img src="../assets/images/liefern_orange.png"  alt="Einkaufen und Liefern">
+           Einkaufen & <br/> Liefern
             <ion-ripple-effect></ion-ripple-effect>
           </div>        
-        </router-link>
+        </router-link>    
 
-
-       
-
+        <br>
         <p>Oder möchtest du<br /><strong> dir etwas liefern lassen?</strong></p>
       
-        <router-link to="/deliveries">
-          <div class="ion-activatable ripple-parent">
+        <router-link to="/orders">
+          <div class="ion-activatable ripple-parent2">
            <img src="../assets/images/bestellen_green.png"  alt="Einkaufen und Liefern">
-           <div class="text_bottom_centered"> Bestellen</div>
+           Bestellen
             <ion-ripple-effect></ion-ripple-effect>
           </div>        
         </router-link>
 
+        <br>
         <p>
           Oder zurück zum
           <router-link to="/onboarding"> Onboarding </router-link>
@@ -43,12 +42,14 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { IonApp, IonContent, IonRippleEffect } from '@ionic/vue';
 import Header from '../components/Header.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
-    Header,
+    Header, IonApp, IonContent, IonRippleEffect
+
   },
 });
 </script>
@@ -58,24 +59,40 @@ h1 {
   font-size: 24px;
   margin-bottom: 20px;
 }
-.button {
-  background-color: none; 
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-}
-.text_bottom_centered{
-  position:relative;
-  bottom: 60px;
-  left: 50%;
-  color: black;
-  transform: translate(-50%, -50%);
-  font-size: 16px;
-}
+.ripple-parent {
+    position: relative;
+    overflow: hidden;
+    background-color: white;
+    width: 200px;
+    height: 170px;
+    border-width: 1px; 
+    border-color: none; 
+    border-radius: 100px 100px 100px 100px; 
+      box-shadow: 0px 3px 3px 1px #FF9E16;
+    color: black;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+  }
+
+  .ripple-parent2 {
+    position: relative;
+    overflow: hidden;
+    background-color: white;
+    width: 200px;
+    height: 170px;
+    border-width: 1px; 
+    border-color: none; 
+    border-radius: 100px 100px 100px 100px; 
+      box-shadow: 0px 3px 3px 1px #60D58E;
+    color: black;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+  }
+
 
 .wrapper {
   text-align: center;
@@ -89,5 +106,7 @@ img {
   &:hover {
     transform: scale(1.05);
   }
+
+
 }
 </style>
