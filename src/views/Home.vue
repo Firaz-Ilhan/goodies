bee3ed17f9a11eec2475545fc2502d30ba4210c2
Chris <ch148@hdm-stuttgart.de>
2022-02-21 20:06:37 +0100

    refactor buttons for home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,33 +4,35 @@
 
     <ion-content>
       <div class="wrapper">
-        <h1>Herzlich Willkommen bei goodies!</h1>
-        <p>Hier helfen Nachbarn Nachbarn.</p>
-        <br/>
-        <p><strong>Triff deine Auswahl:</strong></p>
-        <p>Möchtest du<br /><strong>Deinen Nachbarn unterstützen?</strong></p>
+        <h1>Wie möchtest du goodies nutzen?</h1>
+        <p>Möchtest du<br /><strong>Deine Nachbarn unterstützen?</strong></p>
 
-    
-        <router-link to="/deliveries">
-          <div class="ion-activatable ripple-parent">
-          <img class="deliver_img" src="../assets/images/deliver.png"  alt="Einkaufen und Liefern">
-            <p class="delvier_txt"> Einkaufen & <br/> Liefern</p>
-            <ion-ripple-effect></ion-ripple-effect>
-          </div>        
-        </router-link>    
-        <br/>
-        <br/>
+        <button
+          class="ion-activatable ripple-parent"
+          @click="$router.push('/deliveries')"
+        >
+          <img
+            src="../assets/images/illustration_shopping.svg"
+            alt="Frau mit Einkaufstüte"
+          />
+          <p>Einkaufen &amp; Liefern</p>
+          <ion-ripple-effect></ion-ripple-effect>
+        </button>
+
         <p>Oder möchtest du<br /><strong> dir etwas liefern lassen?</strong></p>
-      
-        <router-link to="/orders">
-          <div class="ion-activatable ripple-parent green-shadow">
-           <img class="order_img" src="../assets/images/order.png"  alt="Einkaufen und Liefern">
-           <p class="order_txt">Bestellen</p>
-            <ion-ripple-effect></ion-ripple-effect>
-          </div>        
-        </router-link>
-        <br/>
-        <br/>
+
+        <button
+          class="ion-activatable ripple-parent green-shadow"
+          @click="$router.push('/orders')"
+        >
+          <img
+            src="../assets/images/illustration_package_delivery.svg"
+            alt="Lieferantin übergibt Paket an Kunden"
+          />
+          <p>Bestellen</p>
+          <ion-ripple-effect></ion-ripple-effect>
+        </button>
+
         <p>
           Oder zurück zum
           <router-link to="/onboarding"> Onboarding </router-link>
@@ -40,81 +42,50 @@
   </ion-page>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { IonContent, IonRippleEffect } from '@ionic/vue';
+<script lang="ts" setup>
+import { IonRippleEffect } from '@ionic/vue';
 import Header from '../components/Header.vue';
-
-export default defineComponent({
-  name: 'Home',
-  components: {
-    Header, IonContent, IonRippleEffect
-
-  },
-});
 </script>
 
 <style scoped lang="scss">
-h1 {
-  font-size: 24px;
-  margin-bottom: 20px;
-}
-.ripple-parent {
-    position: relative;
-    overflow: hidden;
-    width: 200px;
-    height: 170px;
-    border-width: 3px; 
-    border-color: #FF9E16; 
-    border-radius: 100px 100px 100px 100px; 
-    box-shadow: 0px 3px 3px 2px;
-    color: #FF9E16;
-    text-align: center;
-    display: inline-block;
-    font-size: 16px;
-    p{
-    color: var(--ion-color-light-contrast);
-    }
-    }body[color-theme='dark'] .ripple-parent {
-    background-color: var(--ion-color-light);
-    }
-
- .green-shadow {
-    color: #60D58E;    
-    }
-
-.deliver_img{
-  width:70%;
-}
-.delvier_txt{
- position: absolute;
- bottom: 0px;
- left: 50%;
- transform: translate(-50%);
-}
-.order_img{
-  width: 80%;
- }
-.order_txt{
- position: absolute;
- bottom: 0px;
- left: 50%;
- transform: translate(-50%);
-}
-
 .wrapper {
   text-align: center;
   margin-bottom: 40px;
 }
 
-img {
-  margin: 20px 0;
+.ripple-parent {
+  position: relative;
+  overflow: hidden;
   width: 200px;
+  margin: 4px 0 20px;
+  border-radius: 100px;
+  background-color: #fff;
+  box-shadow: 0px 4px 4px;
+  color: #ff9e16;
   transition: 0.3s ease;
   &:hover {
-    transform: scale(1.05);
+    transform: scale(0.96);
+    box-shadow: 0px 3px 3px;
   }
+  p {
+    color: var(--ion-color-light-contrast);
+    font-size: 16px;
+    font-weight: 700;
+    max-width: 100px;
+    height: 32px;
+    margin: 8px auto;
+  }
+  img {
+    height: 120px;
+    padding: 4px 8px;
+  }
+}
 
+body[color-theme='dark'] .ripple-parent {
+  background-color: var(--ion-color-light);
+}
 
+.green-shadow {
+  color: #60d58e;
 }
 </style>
