638e2b5f8885c485dd031c07fe764204e54c1006
mb309 <mb309@hdm-stuttgart.de>
2022-02-21 16:51:09 +0100

    Buttonupdate (now darkmode possible, + ripple-effect)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,31 +6,31 @@
       <div class="wrapper">
         <h1>Herzlich Willkommen bei goodies!</h1>
         <p>Hier helfen Nachbarn Nachbarn.</p>
-
+        <br/>
         <p><strong>Triff deine Auswahl:</strong></p>
         <p>Möchtest du<br /><strong>Deinen Nachbarn unterstützen?</strong></p>
 
     
         <router-link to="/deliveries">
           <div class="ion-activatable ripple-parent">
-          <img src="../assets/images/liefern_orange.png"  alt="Einkaufen und Liefern">
-           Einkaufen & <br/> Liefern
+          <img class="deliver_img" src="../assets/images/deliver.png"  alt="Einkaufen und Liefern">
+            <p class="delvier_txt"> Einkaufen & <br/> Liefern</p>
             <ion-ripple-effect></ion-ripple-effect>
           </div>        
         </router-link>    
-
-        <br>
+        <br/>
+        <br/>
         <p>Oder möchtest du<br /><strong> dir etwas liefern lassen?</strong></p>
       
         <router-link to="/orders">
           <div class="ion-activatable ripple-parent2">
-           <img src="../assets/images/bestellen_green.png"  alt="Einkaufen und Liefern">
-           Bestellen
+           <img class="order_img" src="../assets/images/order.png"  alt="Einkaufen und Liefern">
+           <p class="order_txt">Bestellen</p>
             <ion-ripple-effect></ion-ripple-effect>
           </div>        
         </router-link>
-
-        <br>
+        <br/>
+        <br/>
         <p>
           Oder zurück zum
           <router-link to="/onboarding"> Onboarding </router-link>
@@ -42,13 +42,13 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonApp, IonContent, IonRippleEffect } from '@ionic/vue';
+import { IonContent, IonRippleEffect } from '@ionic/vue';
 import Header from '../components/Header.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
-    Header, IonApp, IonContent, IonRippleEffect
+    Header, IonContent, IonRippleEffect
 
   },
 });
@@ -62,16 +62,15 @@ h1 {
 .ripple-parent {
     position: relative;
     overflow: hidden;
-    background-color: white;
+    background-color: none;
     width: 200px;
     height: 170px;
-    border-width: 1px; 
-    border-color: none; 
+    border-width: 3px; 
+    border-color: #FF9E16; 
     border-radius: 100px 100px 100px 100px; 
-      box-shadow: 0px 3px 3px 1px #FF9E16;
+      box-shadow: 0px 3px 3px 3px #FF9E16;
     color: black;
     text-align: center;
-    text-decoration: none;
     display: inline-block;
     font-size: 16px;
   }
@@ -79,20 +78,36 @@ h1 {
   .ripple-parent2 {
     position: relative;
     overflow: hidden;
-    background-color: white;
+    background-color: none;
     width: 200px;
     height: 170px;
-    border-width: 1px; 
-    border-color: none; 
+    border-width: 3px; 
+    border-color: #60D58E; 
     border-radius: 100px 100px 100px 100px; 
-      box-shadow: 0px 3px 3px 1px #60D58E;
+     box-shadow: 0px 3px 3px 3px #60D58E;
     color: black;
     text-align: center;
-    text-decoration: none;
     display: inline-block;
     font-size: 16px;
   }
-
+.deliver_img{
+  width:70%;
+}
+.delvier_txt{
+ position: absolute;
+ bottom: 0px;
+ left: 50%;
+ transform: translate(-50%);
+}
+.order_img{
+  width: 80%;
+ }
+.order_txt{
+ position: absolute;
+ bottom: 0px;
+ left: 50%;
+ transform: translate(-50%);
+}
 
 .wrapper {
   text-align: center;
